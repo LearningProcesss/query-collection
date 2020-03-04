136372cf4807511f6eb12f17ff413ed221c6cbdb
LearningProcesss <minerva.mattia.4j@gmail.com>
2020-03-04 23:52:45 +0100

    refactoring
    refactoring

--- test/find.test.js
+++ test/find.test.js
@@ -1,4 +1,4 @@
-import collectionQuery from "../source";
+import find from "../source";
 import { getNasaCollection } from "./fixtures/getCollection";
 
 const nasaCollection = getNasaCollection();
@@ -7,13 +7,13 @@ describe("Test library", () => {
   test("should return 30 elements using { pha: y } as query [$and behaviour]", () => {
     const andQuerySingleField = { pha: "Y" };
     expect(
-      collectionQuery.find(nasaCollection, andQuerySingleField).length
+      find(nasaCollection, andQuerySingleField).length
     ).toBe(30);
   });
   test("should return 151 elements using { pha: N, orbit_class: Aten } query [$and behaviour]", () => {
     const andQueryMultipleFields = { pha: "N", orbit_class: "Aten" };
     expect(
-      collectionQuery.find(nasaCollection, andQueryMultipleFields).length
+      find(nasaCollection, andQueryMultipleFields).length
     ).toBe(151);
   });
   test("should return 153 elements using $or $or: [{pha: N, orbit_class: Parabolic Comet}] [$or behaviour]", () => {
@@ -28,7 +28,7 @@ describe("Test library", () => {
       ]
     };
     expect(
-      collectionQuery.find(nasaCollection, orQueryMultipleFields).length
+      find(nasaCollection, orQueryMultipleFields).length
     ).toBe(153);
   });
   test("should $and [$and behaviour]", () => {
@@ -43,7 +43,7 @@ describe("Test library", () => {
       ]
     };
     expect(
-      collectionQuery.find(nasaCollection, andQueryMultipleFields).length
+      find(nasaCollection, andQueryMultipleFields).length
     ).toBe(0);
   });
 });
