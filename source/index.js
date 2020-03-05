2dc9bab40a6dba5dc2077140444e77667bb1433b
LearningProcesss <minerva.mattia.4j@gmail.com>
2020-03-05 11:03:15 +0100

    bring back from feat-1
    bring back from feat-1

--- source/index.js
+++ source/index.js
@@ -1,3 +1,82 @@
-import * as query from "./collection";
+/**
+ * Search inside the provided collection using the query object.
+ * @param {array} - collection - array where to search.
+ * @param {object} - query - query object.
+ * @return { array } array filtered.
+ *
+ * @example find([ { name: "a", surname: "aa" }, { name: "b", surname: "bb" } ], { name: 'a' })
+ */
+function find(collection, query) {
+  if (!query) return collection;
+  if (!collection || collection.length === 0) return collection;
+  //console.log("find --> start");
+  const result = collection.filter(item => filterCallback(item, query));
+  //console.log(`result --> ${result.length}`);
+  return result;
+}
 
-export default query
+function filterCallback(item, query) {
+  /*  console.log(*/
+  //"---------------------------- filterCallback -------------------------"
+  /*);*/
+  let ret = iterate(query, [], item);
+  //console.log(ret);
+  /*console.log(*/
+  //"---------------------------------------------------------------------"
+  /*);*/
+
+  return ret[0].isArray
+    ? ret[0].operator === "$or"
+      ? ret[0].results.some(r => r.results === true)
+      : ret[0].operator === "$and"
+      ? ret[0].results.every(r => r.results === true)
+      : null
+    : ret[0].results;
+}
+
+function iterate(obj, comparisons, item) {
+  /*console.log(*/
+  //`iterate --> START --> obj: ${JSON.stringify(
+  //obj
+  //)} , comparison: ${comparisons} , item: ${JSON.stringify(item)}`
+  /*);*/
+  Object.getOwnPropertyNames(obj).forEach(prop => {
+    //console.log(`iterate --> obj: ${JSON.stringify(obj)} --> prop: ${prop}`);
+    if (Array.isArray(obj[prop]) && ["$or", "$and"].includes(prop)) {
+      //console.log(`iterate --> isArray --> ${JSON.stringify(obj[prop])}`);
+      comparisons.push({
+        name: prop,
+        operator: prop,
+        isArray: true,
+        results: []
+      });
+      obj[prop].forEach(i =>
+        iterate(i, comparisons[comparisons.length - 1].results, item)
+      );
+    } else {
+      comparisons.push({
+        name: prop,
+        operator: "$eq",
+        isArray: false,
+        results: singleComparison(
+          { name: prop, value: item[prop], type: typeof item[prop] },
+          { name: prop, value: obj[prop], type: typeof obj[prop] }
+        )
+      });
+    }
+  });
+
+  return comparisons;
+}
+
+function singleComparison(itemP, queryP) {
+  /*console.log(*/
+  //`singleComparison --> itemP: ${JSON.stringify(
+  //itemP
+  //)} , queryP: ${JSON.stringify(queryP)}`
+  /*);*/
+  if (itemP.type !== queryP.type) return false;
+  return itemP.value === queryP.value ? true : false;
+}
+
+export default find;
--- test/find.test.js
+++ test/find.test.js
@@ -1,19 +1,19 @@
-import query from "../source";
+import find from "../source";
 import { getNasaCollection } from "./fixtures/getCollection";
 
 const nasaCollection = getNasaCollection();
 
 describe("Test library", () => {
-  test("should return 30 elements using { pha: y } as query [$and behaviour]", () => {
+  test("should return 30 elements using { pha: y } as [$and behaviour]", () => {
     const andQuerySingleField = { pha: "Y" };
     expect(
-      query.find(nasaCollection, andQuerySingleField).length
+      find(nasaCollection, andQuerySingleField).length
     ).toBe(30);
   });
-  test("should return 151 elements using { pha: N, orbit_class: Aten } query [$and behaviour]", () => {
+  test("should return 151 elements using { pha: N, orbit_class: Aten } [$and behaviour]", () => {
     const andQueryMultipleFields = { pha: "N", orbit_class: "Aten" };
     expect(
-      query.find(nasaCollection, andQueryMultipleFields).length
+      find(nasaCollection, andQueryMultipleFields).length
     ).toBe(151);
   });
   test("should return 153 elements using $or $or: [{pha: N, orbit_class: Parabolic Comet}] [$or behaviour]", () => {
@@ -28,7 +28,7 @@ describe("Test library", () => {
       ]
     };
     expect(
-      query.find(nasaCollection, orQueryMultipleFields).length
+      find(nasaCollection, orQueryMultipleFields).length
     ).toBe(153);
   });
   test("should $and [$and behaviour]", () => {
@@ -43,7 +43,7 @@ describe("Test library", () => {
       ]
     };
     expect(
-      query.find(nasaCollection, andQueryMultipleFields).length
+      find(nasaCollection, andQueryMultipleFields).length
     ).toBe(0);
   });
 });
