969b3dc90e6776159b835f6b2febc5174ff092e5
LearningProcesss <minerva.mattia.4j@gmail.com>
2020-03-08 23:33:37 +0100

    comparison op
    code refactor + comparison op

--- source/collection/methods/find.js
+++ source/collection/methods/find.js
@@ -7,81 +7,201 @@
  * @example find([ { name: "a", surname: "aa" }, { name: "b", surname: "bb" } ], { name: 'a' })
  */
 function find(collection, query) {
+
   if (!query) return collection;
+
   if (!collection || collection.length === 0) return collection;
+
   const result = collection.filter(item => filterCallback(item, query));
+
   return result;
 }
 
 function filterCallback(item, query) {
-  /*  console.log(*/
-  //"---------------------------- filterCallback -------------------------"
-  /*);*/
+
   let ret = iterate(query, [], item);
-  //console.log(ret);
-  /*console.log(*/
-  //"---------------------------------------------------------------------"
-  /*);*/
 
   return ret[0].isArray
     ? ret[0].operator === "$or"
       ? ret[0].results.some(r => r.results === true)
       : ret[0].operator === "$and"
-      ? ret[0].results.every(r => r.results === true)
-      : null
+        ? ret[0].results.every(r => r.results === true)
+        : null
     : ret[0].results;
 }
 
 function iterate(obj, comparisons, item) {
-  /*console.log(*/
-  //`iterate --> START --> obj: ${JSON.stringify(
-  //obj
-  //)} , comparison: ${comparisons} , item: ${JSON.stringify(item)}`
-  /*);*/
-  Object.getOwnPropertyNames(obj).forEach(prop => {
-    //console.log(`iterate --> obj: ${JSON.stringify(obj)} --> prop: ${prop}`);
-    if (Array.isArray(obj[prop]) && ["$or", "$and"].includes(prop)) {
-      //console.log(`iterate --> isArray --> ${JSON.stringify(obj[prop])}`);
+
+  // console.log(`iterate --> START --> obj: ${JSON.stringify(obj)} , comparison: ${comparisons}`);
+
+  Object.getOwnPropertyNames(obj).forEach(propName => {
+
+    // console.log(`iterate --> propName: ${propName}`);
+
+    const propertyObject = obj[propName];
+
+    const propertyType = variableTypeChecker(propertyObject);
+
+    // console.log(`property type --> ${propertyType}`);
+
+    if (propertyType === "Array" && ["$or", "$and"].includes(propName)) {
+
       comparisons.push({
-        name: prop,
-        operator: prop, // $or,$and
+        name: propName,
+        operator: propName, // $or,$and
         isArray: true,
         results: []
       });
-      obj[prop].forEach(arrayProp =>
+
+      propertyObject.forEach(arrayProp =>
         iterate(arrayProp, comparisons[comparisons.length - 1].results, item)
       );
+    } else if (propertyType === "object" && containsComparisonOperator(propertyObject)) {
+
+      const comparisonOperator = getComparisonOperator(propertyObject); // ["$eq","Parabolic Comet"]
+
+      // console.log(`getComparisonOperator -> ${JSON.stringify(comparisonOperator)}`);
+
+      const comparison = createComparison(propName, comparisonOperator[0]);
+
+      // console.log(`createComparison -> ${JSON.stringify(comparison)}`);
+
+      comparison.results = validateComparison(comparisonOperator[0], {
+        name: propName,
+        value: item[propName],
+        type: variableTypeChecker(item[propName])
+      }, {
+        name: propName,
+        value: propertyObject,
+        type: variableTypeChecker(propertyObject)
+      })
+
+      comparisons.push(comparison);
     } else {
       comparisons.push({
-        name: prop,
+        name: propName,
         operator: "$eq",
         isArray: false,
-        results: singleComparison(
+        results: validateComparison("$eq",
           {
-            name: prop,
-            value: item[prop],
-            type: variableTypeChecker(item[prop])
+            name: propName,
+            value: item[propName],
+            type: variableTypeChecker(item[propName])
           },
           {
-            name: prop,
-            value: obj[prop],
-            type: variableTypeChecker(obj[prop])
+            name: propName,
+            value: propertyObject,
+            type: variableTypeChecker(propertyObject)
           }
         )
       });
     }
+
+    // if (Array.isArray(obj[propName]) && ["$or", "$and"].includes(propName)) {
+    //   comparisons.push({
+    //     name: propName,
+    //     operator: propName, // $or,$and
+    //     isArray: true,
+    //     results: []
+    //   });
+    //   obj[propName].forEach(arrayProp =>
+    //     iterate(arrayProp, comparisons[comparisons.length - 1].results, item)
+    //   );
+    // } else {
+    //   comparisons.push({
+    //     name: propName,
+    //     operator: "$eq",
+    //     isArray: false,
+    //     results: validateComparison(
+    //       "$eq",
+    //       {
+    //         name: propName,
+    //         value: item[propName],
+    //         type: variableTypeChecker(item[propName])
+    //       },
+    //       {
+    //         name: propName,
+    //         value: obj[propName],
+    //         type: variableTypeChecker(obj[propName])
+    //       }
+    //     )
+    //   });
+    // }
   });
 
   return comparisons;
 }
 
-function singleComparison(itemP, queryP) {
-  //console.log(`singleComparison --> itemP: ${JSON.stringify(itemP)} , queryP: ${JSON.stringify(queryP)}`);
+/**
+ * 
+ * @param {Object} object 
+ */
+function containsComparisonOperator(object) {
+  return Object.getOwnPropertyNames(object).some(a1 => ["$eq", "$gt", "$gte", "$in", "$lt", "$lte", "$ne", "$nin"].includes(a1));
+}
+
+/**
+ * 
+ * @param {Object} object 
+ * 
+ * @returns ["key name", "key value"]
+ */
+function getComparisonOperator(object) {
+  return Object.entries(object)[0];
+}
+
+function createComparison(propName, operator, isArray = false) {
+  return {
+    name: propName,
+    operator: operator,
+    isArray: isArray,
+    results: {}
+  }
+}
+
+function validateComparison(operator, itemP, queryP) {
+
+  const comparisonOp = ["$eq", "$gt", "$gte", "$in", "$lt", "$lte", "$ne", "$nin"];
+
+  const evaluationOp = ["$regex"];
+
+  // console.log(`validateComparison --> itemP: ${JSON.stringify(itemP)} , queryP: ${JSON.stringify(queryP)}`);
+
   if (queryP.type === "RegExp" && itemP.type === "string") {
     return queryP.value.test(itemP.value);
+  } 
+  else if (itemP.type !== queryP.type) {
+    return false;
+  } else if (["$gt", "$gte", "$lt", "$lte"].includes(operator) && (itemP.type !== "Number" || queryP.item !== "Number")) {
+    return false;
   }
-  if (itemP.type !== queryP.type) return false;
-  return itemP.value === queryP.value ? true : false;
+
+
+  // return itemP.value === queryP.value ? true : false;
+  return compareComparisonOperator(operator, itemP.value, queryP.value)
+}
+
+
+/**
+ * 
+ * @param {string} operator comparison operator
+ * @param {any} itemValue value of the property of the item inside the array.
+ * @param {any} queryValue value of the property of the query.
+ * 
+ * @returns true or false
+ * 
+ * @example compareComparisonOperator("$eq", "Parabolic Curve", "Parabolic Non Curve") return false
+ */
+function compareComparisonOperator(operator, itemValue, queryValue) {
+  if (operator === "$eq") {
+    return itemValue === queryValue
+  }
+}
+
+function compareEvaluationOperator(operator, itemValue, queryValue) {
+  if(operator === "$regex") {
+    return queryValue.test(itemValue);
+  }
 }
 
 function variableTypeChecker(obj) {
--- test/find.test.js
+++ test/find.test.js
@@ -4,66 +4,75 @@ import { getNasaCollection } from "./fixtures/getCollection";
 const nasaCollection = getNasaCollection();
 
 describe("Test collection method: find", () => {
-  test("should execute regex on string field with case sensitive match, simple query", () => {
-    const regexQuerySingleField = {
-      orbit_class: /te/
+  test("should $eq", () => {
+    const eqOperator = {
+      orbit_class: {
+        $eq: "Parabolic Comet"
+      }
     };
-    const result = find(nasaCollection, regexQuerySingleField);
-
-    expect(result[0].orbit_class).toMatch(/te/);
-  });
-  test("should execute regex ignore case match, simple query", () => {
-    const regexQuerySingleField = {
-      orbit_class: /co/i
-    };
-    const result = find(nasaCollection, regexQuerySingleField);
-
-    expect(result[0].orbit_class).toMatch(/co/i);
-  });
-  test("should find elements whit one of the condtion satisfied", () => {
-    const regexOr = {
-      $or: [{ orbit_class: /te/ }, { pha: "Y" }]
-    };
-    const result = find(nasaCollection, regexOr);
-    expect(result[0].pha).toBe("Y");
-    expect(result[0].orbit_class).not.toMatch(/te/);
-
-    expect(result[5].orbit_class).toMatch(/te/);
-    expect(result[5].pha).not.toBe("Y");
+    const result = find(nasaCollection.slice(1), eqOperator);
   });
 
-  test("should return 30 elements using { pha: y } as [$and behaviour]", () => {
-    const andQuerySingleField = { pha: "Y" };
-    expect(find(nasaCollection, andQuerySingleField).length).toBe(30);
-  });
-  test("should return 151 elements using { pha: N, orbit_class: Aten } [$and behaviour]", () => {
-    const andQueryMultipleFields = { pha: "N", orbit_class: "Aten" };
-    expect(find(nasaCollection, andQueryMultipleFields).length).toBe(151);
-  });
-  test("should return 153 elements using $or $or: [{pha: N, orbit_class: Parabolic Comet}] [$or behaviour]", () => {
-    const orQueryMultipleFields = {
-      $or: [
-        {
-          pha: "N"
-        },
-        {
-          orbit_class: "Parabolic Comet"
-        }
-      ]
-    };
-    expect(find(nasaCollection, orQueryMultipleFields).length).toBe(153);
-  });
-  test("should $and [$and behaviour]", () => {
-    const andQueryMultipleFields = {
-      $and: [
-        {
-          pha: "N"
-        },
-        {
-          orbit_class: "Parabolic Comet"
-        }
-      ]
-    };
-    expect(find(nasaCollection, andQueryMultipleFields).length).toBe(0);
-  });
+  /*  test("should execute regex on string field with case sensitive match, simple query", () => {*/
+  //const regexQuerySingleField = {
+  //orbit_class: /te/
+  //};
+  //const result = find(nasaCollection, regexQuerySingleField);
+
+  //expect(result[0].orbit_class).toMatch(/te/);
+  //});
+  //test("should execute regex ignore case match, simple query", () => {
+  //const regexQuerySingleField = {
+  //orbit_class: /co/i
+  //};
+  //const result = find(nasaCollection, regexQuerySingleField);
+
+  //expect(result[0].orbit_class).toMatch(/co/i);
+  //});
+  //test("should find elements whit one of the condtion satisfied", () => {
+  //const regexOr = {
+  //$or: [{ orbit_class: /te/ }, { pha: "Y" }]
+  //};
+  //const result = find(nasaCollection, regexOr);
+  //expect(result[0].pha).toBe("Y");
+  //expect(result[0].orbit_class).not.toMatch(/te/);
+
+  //expect(result[5].orbit_class).toMatch(/te/);
+  //expect(result[5].pha).not.toBe("Y");
+  //});
+
+  //test("should return 30 elements using { pha: y } as [$and behaviour]", () => {
+  //const andQuerySingleField = { pha: "Y" };
+  //expect(find(nasaCollection, andQuerySingleField).length).toBe(30);
+  //});
+  //test("should return 151 elements using { pha: N, orbit_class: Aten } [$and behaviour]", () => {
+  //const andQueryMultipleFields = { pha: "N", orbit_class: "Aten" };
+  //expect(find(nasaCollection, andQueryMultipleFields).length).toBe(151);
+  //});
+  //test("should return 153 elements using $or $or: [{pha: N, orbit_class: Parabolic Comet}] [$or behaviour]", () => {
+  //const orQueryMultipleFields = {
+  //$or: [
+  //{
+  //pha: "N"
+  //},
+  //{
+  //orbit_class: "Parabolic Comet"
+  //}
+  //]
+  //};
+  //expect(find(nasaCollection, orQueryMultipleFields).length).toBe(153);
+  //});
+  //test("should $and [$and behaviour]", () => {
+  //const andQueryMultipleFields = {
+  //$and: [
+  //{
+  //pha: "N"
+  //},
+  //{
+  //orbit_class: "Parabolic Comet"
+  //}
+  //]
+  //};
+  //expect(find(nasaCollection, andQueryMultipleFields).length).toBe(0);
+  /*});*/
 });
